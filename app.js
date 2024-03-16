681b41da7ec712dc5fa76f3a786b9654f61cbe8d
filippovdo <117165629+filippovdo@users.noreply.github.com>
2024-03-16 03:09:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 
